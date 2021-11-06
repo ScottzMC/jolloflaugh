cc4a86ffe12a5c1fc374a9fe6f7960581165d77d
Rising Phoenix <scottmike275@gmail.com>
2021-11-06 12:47:46 +0000

    Update detail.php

--- application/views/site/detail.php
+++ application/views/site/detail.php
@@ -54,11 +54,24 @@
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+	
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-KN3SJZ3');</script>
+	<!-- End Google Tag Manager -->
 
 </head>
 
 <body class="body-color">
 	<?php include 'menu/nav.php'; ?>
+	
+	 <!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KN3SJZ3"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 	
 	<style type="text/css">
        .mobileHide{ 
