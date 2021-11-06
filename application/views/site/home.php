007081d1b83354179b4feb790e629c99947ef5a9
Rising Phoenix <scottmike275@gmail.com>
2021-11-06 13:11:38 +0000

    Update home.php

--- application/views/site/home.php
+++ application/views/site/home.php
@@ -10,7 +10,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<!-- Site Title- -->
-	<title>Welcome to Ticket Event Platform</title>
+	<title>Welcome to Jollof N Laugh</title>
 
 	<!-- CSS
    ==================================================== -->
