876ed205996d5fc5cc219c33901fec384ba40877
Rising Phoenix <scottmike275@gmail.com>
2021-11-09 21:41:21 +0000

    Update home.php

--- application/views/site/home.php
+++ application/views/site/home.php
@@ -10,7 +10,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<!-- Site Title- -->
-	<title>Welcome to Jollof N Laugh</title>
+	<title>Welcome to Jollof N Laugh Show</title>
 
 	<!-- CSS
    ==================================================== -->
