95c3f185553816e350f2bf01c53fde2d717ac81c
Rising Phoenix <scottmike275@gmail.com>
2021-11-09 22:30:05 +0000

    Update home.php

--- application/views/site/home.php
+++ application/views/site/home.php
@@ -10,7 +10,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<!-- Site Title- -->
-	<title>The Jollof N Laugh Show Eat - Laugh - Dance</title>
+	<title>The Jollof N Laugh Show</title>
 
 	<!-- CSS
    ==================================================== -->
