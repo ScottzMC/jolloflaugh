3968f3798de70ee214e9805261cc045db1c18a42
Rising Phoenix <scottmike275@gmail.com>
2021-11-09 21:51:48 +0000

    Update contact.php

--- application/views/site/contact.php
+++ application/views/site/contact.php
@@ -10,7 +10,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<!-- Site Title- -->
-	<title>Contact Us || Jollof N Laugh</title>
+	<title>Contact Us || The Jollof N Laugh Show</title>
 
 	<!-- CSS
    ==================================================== -->
