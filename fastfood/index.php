39bf4fc51f7cb4dfb8bdc5bc980dac9eb4670c75
Rising Phoenix <scottmike275@gmail.com>
2021-11-28 22:41:37 +0000

    Update index.php

--- fastfood/index.php
+++ fastfood/index.php
@@ -53,8 +53,7 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	//define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development'); // Show error
-	define ("environment", "production"); // default is development and not show error
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
 /*
  *---------------------------------------------------------------
@@ -64,51 +63,31 @@
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
- 
- // Show error
- 
-    /*switch (ENVIRONMENT){
-    	case 'development':
-    		error_reporting(-1);
-    		ini_set('display_errors', 1);
-    	break;
-    
-    	case 'testing':
-    	case 'production':
-    		ini_set('display_errors', 0);
-    		if (version_compare(PHP_VERSION, '5.3', '>='))
-    		{
-    			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-    		}
-    		else
-    		{
-    			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-    		}
-    	break;
-    
-    	default:
-    		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-    		echo 'The application environment is not set correctly.';
-    		exit(1); // EXIT_ERROR
-    }*/
+switch (ENVIRONMENT)
+{
+	case 'development':
+		error_reporting(-1);
+		ini_set('display_errors', 1);
+	break;
 
-    // Not show error
-    if (defined ("environment")){
-        switch (environment){
-            case "development":
-                error_reporting (e_all);
-                break;
-    
-            case "testing":
-            
-            case "production":
-                error_reporting (0);
-                break;
-        
-        default:
-            exit ("the application environment is not set correctly.");
-        }
-    }
+	case 'testing':
+	case 'production':
+		ini_set('display_errors', 0);
+		if (version_compare(PHP_VERSION, '5.3', '>='))
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+		}
+		else
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+		}
+	break;
+
+	default:
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'The application environment is not set correctly.';
+		exit(1); // EXIT_ERROR
+}
 
 /*
  *---------------------------------------------------------------
