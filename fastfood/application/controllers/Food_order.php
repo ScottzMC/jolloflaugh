aa737cdd74f6af5fa2900535abfc540d548600db
Rising Phoenix <scottmike275@gmail.com>
2021-11-30 12:20:35 +0000

    Update Food_order.php

--- fastfood/application/controllers/Food_order.php
+++ fastfood/application/controllers/Food_order.php
@@ -73,7 +73,7 @@
                       window.location.href="<?php echo site_url('food_order'); ?>";
                   </script>-->
                   <?php
-		  echo "<script type=\"text/javascript\">alert('Not working');</script>";
+		  echo '<script>alert("Welcome to Geeks for Geeks")</script>';
 		  header("Location: https://jollofnlaugh.com/fastfood/food_order");
                   //(isset($_SERVER['HTTP_REFERER'])){
                     //redirect($_SERVER['HTTP_REFERER']);
