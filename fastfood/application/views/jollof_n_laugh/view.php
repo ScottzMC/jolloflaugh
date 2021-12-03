1c05876690eb8a8a73c3cd324f45ffc6a6c4228e
Rising Phoenix <scottmike275@gmail.com>
2021-12-03 10:19:44 +0000

    Update view.php

--- fastfood/application/views/jollof_n_laugh/view.php
+++ fastfood/application/views/jollof_n_laugh/view.php
@@ -462,8 +462,7 @@
 
     <!-- All JS is here
 ============================================ -->
-    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
-    <script src="https://jollofnlaugh.com/fastfood/assets/js/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!--<script src="https://jollofnlaugh.com/fastfood/assets/js/vendor/bootstrap.bundle.min.js"></script>-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://jollofnlaugh.com/fastfood/assets/js/plugins/slick.js"></script>
