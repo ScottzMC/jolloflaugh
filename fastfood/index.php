881a10958c9d12ed5931b51f90e22eddcd9edc90
Rising Phoenix <scottmike275@gmail.com>
2021-12-25 01:55:32 +0000

    Update index.php

--- fastfood/index.php
+++ fastfood/index.php
@@ -67,49 +67,31 @@
  
  // Show error
  
-    switch (ENVIRONMENT){
-    	/*case 'development':
-    		error_reporting(-1);
-    		ini_set('display_errors', 1);
-    	break;
-    
-    	case 'testing':
-    	case 'production':
-    		ini_set('display_errors', 0);
-    		if (version_compare(PHP_VERSION, '5.3', '>='))
-    		{
-    			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-    		}
-    		else
-    		{
-    			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-    		}
-    	break;
-    
-    	default:
-    		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-    		echo 'The application environment is not set correctly.';
-    		exit(1); // EXIT_ERROR
-    }*/
+    switch (ENVIRONMENT)
+{
+	case 'development':
+		error_reporting(-1);
+		ini_set('display_errors', 1);
+	break;
 
-    // Not show error
-    if (defined ("environment")){
-        switch (environment){
-            case "development":
-                error_reporting (e_all);
-                break;
-    
-            case "testing":
-            
-            case "production":
-                error_reporting (0);
-                break;
-        
-        default:
-            exit ("the application environment is not set correctly.");
-        }
-    }
-  }
+	case 'testing':
+	case 'production':
+		ini_set('display_errors', 0);
+		if (version_compare(PHP_VERSION, '5.3', '>='))
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+		}
+		else
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+		}
+	break;
+
+	default:
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'The application environment is not set correctly.';
+		exit(1); // EXIT_ERROR
+}
 
 /*
  *---------------------------------------------------------------
